8d8814782793a8e94978c7947f2ba547c1d74d09
Jeremy Foote <jdfoote1@gmail.com>
2011-12-23 11:03:06 -0800

    First commit

---
+++ site/list.js
@@ -0,0 +1,247 @@
+// Set global variables
+		var dataView;
+		var grid;
+
+		var data = [];
+
+// Set up SlickGrid display columns
+		var columns = [
+			{id:"linkedName", name:"Name", field:"linkedName", cssClass:"cell-title", width:150, sortable: true},
+			{id:"gender", name:"Gender", field:"gender", width:45},
+			{id:"birthDate", name:"Birth Date", field:"birthDate"},
+			{id:"birthPlace", name:"Birth Place", field:"birthPlace", width:150, sortable: false},
+			{id:"deathDate", name:"Death Date", field:"deathDate"},
+			{id:"deathPlace", name:"Death Place", field:"deathPlace", width: 150, sortable: false}
+		];
+
+// Set SlickGrid options
+		var options = {
+			editable: false,
+			enableAddRow: false,
+			enableCellNavigation: true,
+			asyncEditorLoading: false,
+			forceFitColumns: true
+		};
+
+
+		var searchString = "";
+
+// Define search filter (currently searches name, birth place, and death place)
+		function myFilter(item) {
+
+			var searchFields = ["name","birthPlace","deathPlace"];
+			if (searchString !== ""){
+				searchString = searchString.toUpperCase();
+				var itemFound = false;
+				for (i in searchFields){
+					if (item[searchFields[i]].toUpperCase().indexOf(searchString) != -1){
+						itemFound = true;
+					}
+				}
+				if (itemFound === false){
+					return false;
+				}
+			}
+
+// This is from the original example, and I'm not sure that I understand what it's doing. I think we will need code like this to filter lists that have parents.
+// In the original example, rows can have parents that they're nested under; the rows are hidden if the parents are hidden. We don't need this
+//			var idx = dataView.getIdxById(item.id);
+//
+//			if (item.parent != null) {
+//				var parent = data[item.parent];
+//
+//				while (parent) {
+//					if (parent._collapsed || (searchString != "" && parent["title"].indexOf(searchString) == -1) )	
+//						return false;
+//
+//					parent = data[parent.parent];
+//				}
+//			}
+//
+			return true;
+		}
+
+// This is my function to do a sort by name any time we change the data.
+
+function initialSort(){
+	sortcol = "linkedName";
+	dataView.sort(comparer, 1);
+}
+
+// This is the comparer. Right now, it just sorts based on the name.
+	function comparer(a,b) {
+		if (sortcol === "linkedName"){
+		var x = a["name"], y = b["name"];
+		return (x == y ? 0 : (x > y ? 1 : -1));
+	}
+} 
+
+		
+	// This function takes a user, and returns a list of their active trees.
+	function getTreeList(user){
+// need the www so we pick up the sign-in cookie
+		var jsonURL = 'http://www.werelate.org/w/index.php?action=ajax&rs=wfGetTrees&user=' + user + '&callback=?';
+		$.getJSON(jsonURL,function(json){
+		// Set up the select menu
+		$('#treeFilter').append('<label>Select Tree: </label><select id="treeSelect"><option value="wfWatchlist" selected="selected">All Trees</option></select>');
+// pick up user from the closure (because I removed the global user variable)
+		$('#treeSelect').change(function() {
+			loadNewTree(user, this.value);
+		});
+		$.each(json, function(key,text){
+			// Create an option in the select menu for each tree
+			$('#treeSelect').append('<option value="' + text + '">' + text + '</option>')
+		});
+	});
+}
+
+// This function loads a new tree from the dropdown list.
+function loadNewTree(user, treeName){
+	var newData = [];
+	// Check if it's asking for a tree, or for the full watchlist
+	if (treeName != "wfWatchlist"){
+// need the www so we pick up the sign-in cookie
+		var jsonURL = 'http://www.werelate.org/w/index.php?action=ajax&rs=wfGetTree&user=' + user + '&tree=' + treeName + '&callback=?';
+		$.getJSON(jsonURL,function(json){
+		var treePeople = [];
+		// Go through each item and put it into the treePeople array
+	  $.each(json, function(key,val) {
+		  treePeople.push(val);
+		 });
+		// Compare the treePeople array with the current data. Add the people who are in the tree to newData
+		for(var i=0; i<data.length; i++){
+			if ($.inArray(data[i].title,treePeople) != -1){
+				newData.push(data[i]);
+			}
+		}
+		// Update the data in the grid, and then update the grid
+		dataView.setItems(newData);
+		grid.updateRowCount();
+		grid.render();
+		initialSort();
+});
+}
+else { // If they want the whole watchlist, then reset to the original data.
+	dataView.setItems(data);
+	grid.updateRowCount();
+	grid.render();
+	initialSort();
+}
+}
+
+	// This function takes a user name and tree name, loads the data, and initializes the grid. This has to be called before the grid shows up.
+	function loadTree(user){
+// need the www so we pick up the sign-in cookie
+		var jsonURL = 'http://www.werelate.org/w/index.php?action=ajax&rs=wfGetWatchlist&user=' + user + '&callback=?';
+		$.getJSON(jsonURL,function(json){
+	var items = [];
+	// Go through each item and put it into the data object
+  $.each(json, function(key, val) {
+	  var d = (data[key] = {});
+// We don't need parent or indent because we're not expanding/collapsing rows
+//	  var parent = null;
+//	  var indent = 0;
+				d["id"] = "id_" + key;
+//				d["indent"] = indent;
+//				// This refers to whether a node has a parent in the grid
+//				d["parent"] = parent;
+				d["title"] = val.title;
+				// This is used for searching, not display
+// surname and given name may be missing; default to "" like other fields
+				d["name"] = (val.surname||"") + ", " + (val.given||"");
+				d["linkedName"] = '<a href="http://www.werelate.org/wiki/Person:' + val.title + '">' + (val.surname||"") + ', ' + (val.given||"") + '</a>'||"";
+				d["gender"] = val.gender||"";
+				d["birthDate"] = val.birthDate||"";
+				d["birthPlace"] = val.birthPlace||"";
+				d["deathDate"] = val.deathDate||"";
+				d["deathPlace"] = val.deathPlace||"";
+			});
+
+
+
+// Everything below here is copied directly from the SlickGrid example
+
+			// initialize the model
+			dataView = new Slick.Data.DataView();
+			dataView.beginUpdate();
+			dataView.setItems(data);
+			dataView.setFilter(myFilter);
+			dataView.endUpdate();
+
+
+			// initialize the grid
+			grid = new Slick.Grid("#myGrid", dataView, columns, options);
+
+// We don't need this since we're not allowing cell editing
+//            grid.onCellChange.subscribe(function(e,args) {
+//                dataView.updateItem(args.item.id,args.item);
+//            });
+            
+// We don't need something like this until we allow people to click on a star to flag a row (like gmail)
+//			grid.onClick.subscribe(function(e,args) {
+//                if ($(e.target).hasClass("toggle")) {
+//                    var item = dataView.getItem(args.row);
+//                    if (item) {
+//                        if (!item._collapsed)
+//                            item._collapsed = true;
+//                        else
+//                            item._collapsed = false;
+//
+//                        dataView.updateItem(item.id, item);
+//                    }
+//                    e.stopImmediatePropagation();
+//                }
+//            });
+            
+
+         grid.onSort.subscribe(function(e, args) {
+			sortdir = args.sortAsc ? 1 : -1;
+			sortcol = args.sortCol.field; 
+			dataView.sort(comparer, args.sortAsc);
+		});
+
+
+			// wire up model events to drive the grid
+			dataView.onRowCountChanged.subscribe(function(e,args) {
+				grid.updateRowCount();
+                grid.render();
+			});
+
+			dataView.onRowsChanged.subscribe(function(e,args) {
+				grid.invalidateRows(args.rows);
+				grid.render();
+			});
+
+
+			var h_runfilters = null;
+
+
+			// wire up the search textbox to apply the filter to the model
+			$("#txtSearch").keyup(function(e) {
+                Slick.GlobalEditorLock.cancelCurrentEdit();
+
+				// clear on Esc
+				if (e.which == 27)
+					this.value = "";
+
+				searchString = this.value;
+				dataView.refresh();
+			})
+		initialSort(); // Ok - this is mine, not from SlickGrid.
+		})
+		};
+
+// Ready, set, go.
+$(function()
+		{
+// Get user from url: user parameter for testing, or whatever follows Special:List/ for production
+// default is empty, which returns logged-in user
+			var user = '';
+			var results = new RegExp('(Special:ListPages/|\\?user=)([^&#]*)').exec(window.location.href);
+			if (results) {
+				user = results[2];
+			}
+			loadTree(user);
+			getTreeList(user);
+		}
+		);
